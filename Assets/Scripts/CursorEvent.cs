7d42c99840608d827df23d5ea09a712138ff2953
NightRaider <Jeff.Fan.He@gmail.com>
2016-09-01 11:40:00 +0800

    Smooth movement, no clutch mechanism implemented yet. Next step: change material with gestrue is triggered.

--- Assets/Scripts/CursorEvent.cs
+++ Assets/Scripts/CursorEvent.cs
@@ -1,15 +1,72 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
-public class CursorEvent : MonoBehaviour {
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+/*************************************************************
+Check Pose for armband. Make sure execute before CursorMove and
+other scripts that needs Poses
+*************************************************************/
+public class CursorEvent : MonoBehaviour
+{
+    public GameObject myo = null;
+
+    //enum for all poses, use bitmask to test state in other scrips
+    [Flags]
+    public enum MyoPoses
+    {
+        Idle = 0,
+        MakeFist = 1,
+        FingerSpread = 2,
+        WaveIn = 4,
+        WaveOut = 8
+    }
+
+
+    private Pose _lastPose = Pose.Unknown;
+
+
+    // Use this for initialization
+
+    // Update is called once per frame
+    void Update()
+    {
+        ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo>();
+        if (thalmicMyo.pose != _lastPose)
+        {
+            _lastPose = thalmicMyo.pose;
+            if (thalmicMyo.pose == Pose.Fist)
+            {
+                
+            }
+            //else if (thalmicMyo.pose == Pose.WaveIn)
+            //{
+            //    GetComponent<Renderer>().material = waveInMaterial;
+
+            //    ExtendUnlockAndNotifyUserAction(thalmicMyo);
+            //}
+            //else if (thalmicMyo.pose == Pose.WaveOut)
+            //{
+            //    GetComponent<Renderer>().material = waveOutMaterial;
+
+            //    ExtendUnlockAndNotifyUserAction(thalmicMyo);
+            //}
+            //else if (thalmicMyo.pose == Pose.DoubleTap)
+            //{
+            //    GetComponent<Renderer>().material = doubleTapMaterial;
+
+            //    ExtendUnlockAndNotifyUserAction(thalmicMyo);
+            //}
+
+        }
+
+        //private IEnumerator GearOn(IEnumerator coroutine)
+        //{
+
+        //}
+    }
 }
--- Assets/Scripts/CursorMove.cs
+++ Assets/Scripts/CursorMove.cs
@@ -1,30 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CursorMove : MonoBehaviour {
+/**************************************************
+    Attach to cursor object
+**************************************************/
+
+public class CursorMove : MonoBehaviour
+{
 
     public Transform stick;
-    public float screenScale=1.0f;
-    public float smoothing=1.0f;
-    public int startZ=2000;
+    public float screenScale = 1.0f;
+    public float smoothing = 1.0f;
+    public int startZ = 1999;
 
-	void Update ()
+    private bool cursorMoveActive;
+
+    public bool CursorMoveActive
     {
-        Vector3 projXY= new Vector3(stick.position.x, stick.position.y, 0);
-        Vector3 targetZ = new Vector3(0, 0, startZ);
-        if (Vector3.Distance(transform.position, screenScale * projXY+targetZ) > 0.05f)
+        get { return cursorMoveActive; }
+        set { cursorMoveActive = value; }
+    }
+
+    public void ToggleActive()
+    {
+        cursorMoveActive = !cursorMoveActive;
+    }
+
+    void Start()
+    {
+        CursorMoveActive = true;
+    }
+
+    void Update()
+    {
+        if (cursorMoveActive)
         {
-            transform.position = Vector3.Lerp(transform.position, screenScale * projXY + targetZ, smoothing*Time.deltaTime);
+            Vector3 projXY = new Vector3(stick.position.x, stick.position.y, 0);
+            Vector3 targetZ = new Vector3(0, 0, startZ);
+            if (Vector3.Distance(transform.position, screenScale * projXY + targetZ) > 0.05f)
+            {
+                transform.position = Vector3.Lerp(transform.position, screenScale * projXY + targetZ, smoothing * Time.deltaTime);
+            }
         }
     }
-	
-	// Update is called once per frame
-	//IEnumerator MoveCursor (Transform target)
- //   {
- //       while (Vector3.Distance(transform.position, screenScale*(new Vector3(target.position.x,target.position.y,0.0f))) > 0.05f)
- //       {
- //           transform.position = Vector3.Lerp(transform.position, screenScale * (new Vector3(target.position.x, target.position.y, 0.0f)), Time.deltaTime);
- //           yield return null;
- //       }
- //   }
+
 }
